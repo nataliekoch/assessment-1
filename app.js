8919c3743a5580a1c044fbd14ffbdbd97cf38e19
nataliekoch <natalie.koch927@gmail.com>
2015-10-24 13:50:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,9 +27,6 @@ function appendDom(){
 	
 			$("#container").append("<div class='propertyid color', </div>");
 			var $el = $("#container").children().last();
-			$el.append("<div>Property ID: " + randomNumber(0, 1000) + "</div>");
-			$el.append("<div>" + randomNumber(1000, 9999) +  " Sq. Ft.</div>");
-			$el.append("<div>$" + randomNumber(1, 50) +  " /Sq. Ft.</div>");
 			$el.append("<button class='deleteButton'>Remove</button>")
 			$el.append("<button class='changeButton'>Change</button>")
 
@@ -37,6 +34,3 @@ function appendDom(){
 				.slideDown(500);
 			
 }
-function randomNumber(min, max){
-	return Math.floor(Math.random() * (1 + max - min) + min);
-};
